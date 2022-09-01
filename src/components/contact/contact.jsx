0276f76396fa233f4d02bf97e67b85f4c35e50d9
Matías Cellamare <88092713+cellamarematias@users.noreply.github.com>
2022-08-31 22:46:55 -0300

    feature/first settings

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import emailjs from 'emailjs-com';
+import './contact.css';
+
+export default function ContactUs() {
+
+  function sendEmail(e) {
+    e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+
+    emailjs.sendForm('service_hwto52n', "template_1t6680l", e.target, 'r2t6UAfzn1d0zaKPB')
+      .then((result) => {
+          window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+		<div className='contactContainer'>
+			<form className="contactForm" onSubmit={sendEmail}>
+				<input type="hidden" name="contact_number" />
+				<label>Name</label>
+				<input type="text" name="from_name" />
+				<label>Email</label>
+				<input type="email" name="from_email" />
+				<label>Subject</label>
+				<input type="text" name="subject" />
+				<label>Message</label>
+				<textarea name="html_message" />
+				<input type="submit" value="Send" />
+			</form>
+		</div>
+  );
+}
---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,100 @@
+import './projects.css';
+import { GrReactjs } from "react-icons/gr";
+import { SiMongodb, SiNodedotjs, SiJavascript, SiMysql } from "react-icons/si";
+
+const Projects = () => {
+	return (
+			<div id='projects' className="projectsContainer">
+				<div className='cardContainer'>
+					<div className='card'>
+						<div className='cardImage'></div>
+						<div className='cardText'>
+							<span className='date'>2022</span>
+							<h2>Expenses App</h2>
+							<p>React</p>
+						</div>
+						<div className='cardStats'>
+							<div className='stat'>
+							<div className='value'><SiJavascript/></div>
+							</div>
+							<div className='stat'>
+								<div className='value'><GrReactjs/></div>
+							</div>
+							<div className='stat'>
+							<div className='value'><SiMongodb/></div>
+							</div>
+						</div>
+					</div>
+					<div className='card'>
+						<div className='cardImageTwo'></div>
+						<div className='cardText'>
+							<span className='date'>2022</span>
+							<h2>API Rest</h2>
+							<p>Express</p>
+						</div>
+						<div className='cardStats'>
+							<div className='stat'>
+							<div className='value'><SiNodedotjs/></div>
+							</div>
+							<div className='stat'>
+								<div className='value'><SiJavascript/></div>
+							</div>
+							<div className='stat'>
+							<div className='value'><SiMongodb/></div>
+							</div>
+						</div>
+					</div>
+					<div className='card'>
+						<div className='cardImageThree'></div>
+						<div className='cardText'>
+							<span className='date'>2021</span>
+							<h2>Node-MySQL</h2>
+							<p>Node</p>
+						</div>
+						<div className='cardStats'>
+							<div className='stat'>
+							<div className='value'><SiNodedotjs/></div>
+							</div>
+							<div className='stat'>
+								<div className='value'><GrReactjs/></div>
+							</div>
+							<div className='stat'>
+							<div className='value'><SiMysql/></div>
+							</div>
+						</div>
+					</div>
+					<div className='card'>
+						<div className='cardImageFour'></div>
+						<div className='cardText'>
+							<span className='date'>2022</span>
+							<h2>Mobile App</h2>
+							<p>React Native</p>
+						</div>
+						<div className='cardStats'>
+							<div className='stat'>
+							<div className='value'><SiNodedotjs/></div>
+							</div>
+							<div className='stat'>
+								<div className='value'><GrReactjs/></div>
+							</div>
+							<div className='stat'>
+							<div className='value'><SiMongodb/></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="arrowContainerProjects">
+					<div class="roundProjects">
+						<a href="#body">
+							<div id="cta">
+									<span class="arrow primera next"></span>
+									<span class="arrow segunda next"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+	)
+}
+
+export default Projects;
